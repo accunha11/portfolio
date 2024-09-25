7e7bd3c0df200f5722ccd0700c7aaa6ec11c2079
accunha11 <anacrcunha00@gmail.com>
2024-09-25 19:29:53 +0100

    icon fix pt3

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,31 @@
  */
 
 // You can delete this file if you're not using it
+
+const React = require('react');
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      key="favicon-32x32"
+      rel="icon"
+      href="/favicon-32x32.png?v=82386017e0ae2213e84c51cd2b931f89"
+      type="image/png"
+      sizes="32x32"
+    />,
+    <link
+      key="favicon-16x16"
+      rel="icon"
+      href="/favicon-16x16.png?v=82386017e0ae2213e84c51cd2b931f89"
+      type="image/png"
+      sizes="16x16"
+    />,
+    <link key="favicon-ico" rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />,
+    <link
+      key="apple-touch-icon"
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/apple-touch-icon.png"
+    />,
+  ]);
+};
